6f99b4840d4097fde7685524d6d203c638199e1f
mnr.mageu <thabondlovu505@gmail.com>
2026-09-03 18:02:30 +0200

    I created a display of data types on python

---
+++ data_types.py
@@ -0,0 +1,13 @@
+# Name--- string
+# Age--- integer
+# Salary--- float
+
+# Variable--- storage for data
+
+Name = "Thabo"
+Age = 19
+Salary = 2000.20
+
+print("This is",Name)
+print("I am",Age)
+print("This is my",Salary)
